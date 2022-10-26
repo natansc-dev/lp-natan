0554aea1016dfe16cec105eb07a3ab6ec2273312
Natan Cardoso <natansc73@gmail.com>
2022-10-25 21:57:53 -0300

    Font Roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-
         <title>Natan Cardoso - Desenvolvedor</title>
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
